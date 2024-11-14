122278a60fa4b4e4753df7e97cdae6f1da9e6a86
Luis Calero <lcalero@z2r8p2.42lyon.fr>
2024-11-14 12:28:29 +0100

    corrected and tested ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,11 @@
 /*   By: lcalero <lcalero@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 16:05:53 by lcalero           #+#    #+#             */
-/*   Updated: 2024/11/13 18:13:00 by lcalero          ###   ########.fr       */
+/*   Updated: 2024/11/14 12:27:31 by lcalero          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 static int	is_sep(char a, char c)
 {
@@ -27,12 +26,12 @@ static int	count_words(char const *s, char c)
 
 	i = 0;
 	cpt = 0;
-	if (is_sep(s[0], c))
-		cpt++;
 	while (s[i])
 	{
 		while (is_sep(s[i], c) && s[i])
 			i++;
+		if (s[i] == '\0')
+			break ;
 		cpt++;
 		while (!is_sep(s[i], c) && s[i])
 			i++;
@@ -46,7 +45,7 @@ static char	*ft_strdup_size(const char *s, unsigned int start, unsigned int end)
 	int		i;
 
 	i = 0;
-	res = malloc(end - start * sizeof(char) + 2);
+	res = (char *)malloc((end - start + 1) * sizeof(char));
 	if (res == NULL)
 		return (NULL);
 	while (s[i] && start + i < end)
@@ -67,33 +66,39 @@ char	**ft_split(char const *s, char c)
 
 	i = 0;
 	k = 0;
-	printf("%d", count_words(s, c));
-	res = malloc(count_words(s, c) * sizeof(char *) + 1);
+	res = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
 	if (res == NULL)
 		return (NULL);
-	while(s[i])
+	while (s[i])
 	{
 		while (is_sep(s[i], c) && s[i])
 			i++;
 		start = i;
 		while (!is_sep(s[i], c) && s[i])
 			i++;
-		res[k] = ft_strdup_size(s, start, i);
-		k++;
+		if (start != i)
+		{
+			res[k] = ft_strdup_size(s, start, i);
+			k++;
+		}
 	}
-	res[k] = 0;
+	res[k] = NULL;
 	return (res);
 }
 
-int main()
+/*int main(int ac, char **av)
 {
+	if (ac != 3)
+		return (1);
 	int	i = 0;
-	char **res = ft_split("split  ||this|for|me|||||!|", '|');
-	while (res[i] != 0)
+	char **res = ft_split(av[1], av[2][0]);
+	while (res[i] != NULL)
 	{
-		printf("%sq", res[i]);
+		printf("%s\n", res[i]);
+		free(res[i]);
 		i++;
 	}
+	printf("%s\n", res[i]);
 	free(res);
 	return (0);
-}
+}*/
